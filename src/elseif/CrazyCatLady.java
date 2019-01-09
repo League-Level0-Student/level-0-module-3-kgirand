a0df30fee69a1d1339efb1f3f2bd450da5b42763
tlcrack <league@WTS5.attlocal.net>
2019-01-08 19:54:05 -0800

    mod 3
    started mod 3

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,13 +5,24 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String amtofcats = JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
-
+int amtcats = Integer.parseInt(amtofcats);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+if(amtcats>=3) {
+	JOptionPane.showMessageDialog(null, "You are a crazy cat lady");
+}
+else if(amtcats==0) {
+	playVideo("https://www.youtube.com/watch?v=ZJT9CeEhM10");
+}
+else if(amtcats < 3 && amtcats > 0) {
+	playVideo("https://www.youtube.com/watch?v=pOmu0LtcI6Y");
+}
 
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,15 +4,29 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		
+		
 		//1. Create a new Robot
-		
+		Robot randy=new Robot();
 		//3. Ask the user what color they would like the robot to draw
-		
+		String input = JOptionPane.showInputDialog("What color do you want the robot to draw? Pink, Green, or Red");
 		//5. Use an if/else statement to set the pen color that the user requested
+if(input.equals("Green")) {
+randy.setPenColor(Color.green);	
+}
+else if(input.equals("Pink")) {
+randy.setPenColor(Color.pink);	
+}
+else if(input.equals("Red")) {
+randy.setPenColor(Color.red);	
+}
 
         //6. If the user doesn’t enter anything, choose a random color
 
@@ -21,7 +35,11 @@ public class RobotColorChooser {
 		//4. Set the pen width to 10
 		
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
+for(int i=0;i<5;i++) {
+	randy.penDown();
+	randy.turn(144);
+	randy.move(100);
+		}
 
 	}
 }
